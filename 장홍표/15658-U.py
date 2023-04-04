25f57559a021097ad96772aa6d73664794ff1c07
remerer <remerer@gmail.com>
2023-04-04 10:08:21 +0900

    change directory

--- 장홍표/15658-U.py
+++
@@ -1,5 +0,0 @@
-import sys
-input = sys.stdin.readline
-#연산자 끼워넣기 - Unsolved
-
-array = []
--- 장홍표/5014.py
+++
@@ -1,6 +0,0 @@
-import sys
-input = sys.stdin.readline
-from collections import deque
-#Startlink
-
-F, S, G, U, D = map(int, input().split())
